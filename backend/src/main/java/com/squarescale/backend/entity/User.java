5a97913490343ea31975bd0c56ce1a7b00c50efe
HotHANDZ <Bwayo115@gmail.com>
2026-03-03 16:28:42 -0500

    Added repository file for admins to find users by usernames and expired passwords

--- backend/src/main/java/com/squarescale/backend/entity/User.java
+++ backend/src/main/java/com/squarescale/backend/entity/User.java
@@ -49,4 +49,20 @@ public class User {
         this.passwordLastSet = LocalDateTime.now();
     }
 
+
+    // GETTERS AND SETTERS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    public Long getId() { return id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+    
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+
 }//END OF CLASS
