094718bbfb54a7aa81ad5b58f157925347f5f3f5
HotHANDZ <Bwayo115@gmail.com>
2026-03-03 18:08:31 -0500

    Added a rough backend with ability to communicate with database

--- backend/src/main/java/com/squarescale/backend/entity/User.java
+++ backend/src/main/java/com/squarescale/backend/entity/User.java
@@ -42,27 +42,54 @@ public class User {
         
         this.firstName = firstName;
         this.lastName = lastName;
+
         this.username = username;
         this.password = password;
+
         this.email = email;
+
         this.role = role;
+
+        
         this.passwordLastSet = LocalDateTime.now();
     }
 
 
     // GETTERS AND SETTERS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    
+    //Identification number
     public Long getId() { return id; }
 
+    //Username
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
+    //Password
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
+    //Role
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
     
+    //Active status
     public boolean isActive() { return active; }
     public void setActive(boolean active) { this.active = active; }
+
+    //First Name
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    //Last Name
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    //Email
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    //Date
+    public LocalDateTime getSuspendedUntil() { return suspendedUntil; }
+    public void setSuspendedUntil(LocalDateTime suspendedUntil) { this.suspendedUntil = suspendedUntil; }
 
 }//END OF CLASS
