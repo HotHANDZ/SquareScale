34d978da01335e580e676bff8c228462b49d5e7d
HotHANDZ <Bwayo115@gmail.com>
2026-03-03 14:26:51 -0500

    Added springboot-ready backend folder.  Added 'database' folder and moved database items into it.  Tested endpoint and verified springboot is working.

---
+++ backend/src/main/java/com/squarescale/backend/SquarescaleApplication.java
@@ -0,0 +1,13 @@
+package com.squarescale.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarescaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquarescaleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/squarescale/backend/SquarescaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squarescale.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarescaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
